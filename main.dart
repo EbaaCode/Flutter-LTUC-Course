c7c54fb2eb8077d2ca000d74d0227f2341aa445f
EbaaCode <62425957+EbaaCode@users.noreply.github.com>
2022-08-25 17:47:48 +0300

    Lab project - 01

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+  Map userInfo = {
+    "firstName":"Ebaa",
+    "age":18,
+    "education":{
+      "collage":"LTUC",
+      "faculty":"ASAC",
+      "year":"2022"
+    },
+    "hobbies":["programming","art","video games"]
+  };
+}
