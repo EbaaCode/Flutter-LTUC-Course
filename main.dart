452587598b897bfc0aac6b69e303c523b93a634a
EbaaCode <62425957+EbaaCode@users.noreply.github.com>
2022-08-28 12:24:53 +0300

    Assignment project - 01

--- main.dart
+++ main.dart
@@ -1,12 +1,12 @@
 void main() {
-  Map userInfo = {
-    "firstName":"Ebaa",
-    "age":18,
-    "education":{
-      "collage":"LTUC",
-      "faculty":"ASAC",
-      "year":"2022"
-    },
-    "hobbies":["programming","art","video games"]
-  };
+  List colors = ["red", "blue", "green", "black", "white", "aqua", "lime"];
+  for (int i = 0; i < colors.length; i++) {
+    if (colors[i][0] == "a") {
+      print(colors[i].toUpperCase());
+    } else if (colors[i][0] == "b") {
+      print(colors[i].toUpperCase());
+    } else {
+      print("Sorry the color ${colors[i]} only contains ${colors[i].length} characters and it doesn't start with A or B.");
+    }
+  }
 }
