7e730352f6bed3cc2a3dc9eed60e9f4acdbb8c2c
EbaaCode <62425957+EbaaCode@users.noreply.github.com>
2022-09-01 16:16:23 +0300

    Lab project - 02

--- cardapp/lib/main.dart
+++ cardapp/lib/main.dart
@@ -1,41 +1,128 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        //! APP BAR
         appBar: AppBar(
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blue,
+          shadowColor: Colors.white,
         ),
-        body: Container(
-          margin: const EdgeInsets.all(30),
-          alignment: Alignment.center,
-          decoration:  BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(25)
-          ),
-          child: const Card(
-            margin: EdgeInsets.all(25),
-            shadowColor: Colors.black38,
-            elevation: 100,
-            color: Colors.redAccent,
-            child: Text(
-              "Happy Birthday!",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-                fontWeight: FontWeight.bold
-              )
+        body: SingleChildScrollView(
+          //! BACKGROUND
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
             ),
+            child: Column(children: [
+              const Image(
+                image: NetworkImage(
+                    "https://media.discordapp.net/attachments/922205499389280337/1014876480280461352/unknown.png?width=355&height=473"),
+                alignment: Alignment.center,
+                fit: BoxFit.fitWidth,
+                width: double.infinity,
+                height: 250,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(15.0),
+                    padding: const EdgeInsets.all(3.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.amber, width: 3),
+                    ),
+                    child: const Text(
+                      "Kitty",
+                      style: TextStyle(fontSize: 20, color: Colors.amberAccent),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(15.0),
+                    padding: const EdgeInsets.all(3.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red, width: 3),
+                    ),
+                    child: const Text(
+                      "2 Months",
+                      style: TextStyle(fontSize: 20, color: Colors.redAccent),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(15.0),
+                    padding: const EdgeInsets.all(3.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.cyan, width: 3),
+                    ),
+                    child: const Text(
+                      "Female",
+                      style: TextStyle(fontSize: 20, color: Colors.cyanAccent),
+                    ),
+                  ),
+                ],
+              ),
+              //! CONTAINER #2
+              Container(
+                margin: const EdgeInsets.all(30.0),
+                child: const Image(
+                  image: NetworkImage(
+                      "https://media.discordapp.net/attachments/922205499389280337/1014876480280461352/unknown.png?width=355&height=473"),
+                  alignment: Alignment.center,
+                  fit: BoxFit.fitWidth,
+                  width: double.infinity,
+                  height: 250,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(15.0),
+                    padding: const EdgeInsets.all(3.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.amber, width: 3),
+                    ),
+                    child: const Text(
+                      "Kitty",
+                      style: TextStyle(fontSize: 20, color: Colors.amberAccent),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(15.0),
+                    padding: const EdgeInsets.all(3.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red, width: 3),
+                    ),
+                    child: const Text(
+                      "2 Months",
+                      style: TextStyle(fontSize: 20, color: Colors.redAccent),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(15.0),
+                    padding: const EdgeInsets.all(3.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.cyan, width: 3),
+                    ),
+                    child: const Text(
+                      "Female",
+                      style: TextStyle(fontSize: 20, color: Colors.cyanAccent),
+                    ),
+                  ),
+                ],
+              ),
+            ]),
           ),
-        )
+        ),
       ),
     );
   }
