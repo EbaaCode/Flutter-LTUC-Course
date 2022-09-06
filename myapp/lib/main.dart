8bb7aa7898cfe4b30c181e419035b140857b7115
EbaaCode <62425957+EbaaCode@users.noreply.github.com>
2022-09-06 12:52:47 +0300

    Assignment - 04

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -9,49 +9,92 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return const Home();
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool flutterCourse = false;
+  bool reactCourse = false;
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 149, 107, 240),
-        ),
+        appBar: AppBar(),
         body: Container(
-          child: Center(
-            child: Column(
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(10, 10),
-                      textStyle: const TextStyle(fontSize: 30),
-                      shadowColor: Colors.red,
-                      elevation: 10,
-                      primary: Colors.red,
-                      onPrimary: Colors.redAccent),
-                  onPressed: () {
-                    print("Hello from the 1st button.");
-                  },
-                  child: const Text(
-                    "1st Button",
-                    style: TextStyle(color: Colors.white),
-                  ),
+          child: Column(
+            children: [
+              const Divider(height: 50),
+              //? Title
+              const Text(
+                "Please select your course!",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w300),
+              ),
+              const Divider(height: 50),
+              //! CHECKBOX #1
+              CheckboxListTile(
+                secondary: const Icon(
+                  Icons.book,
+                  color: Colors.black12,
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(10, 10),
-                      textStyle: const TextStyle(fontSize: 25),
-                      shadowColor: Colors.purple,
-                      elevation: 15,
-                      primary: Colors.purple,
-                      onPrimary: Colors.purpleAccent),
-                  onPressed: () {
-                    print("Hello from the 2nd button.");
-                  },
-                  child: const Text(
-                    "2nd Button",
-                    style: TextStyle(color: Colors.white),
-                  ),
+                subtitle: const Text("Based on the dart programming language.",
+                    style: TextStyle(fontSize: 20, color: Colors.black54)),
+                title: const Text(
+                  "Flutter",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
                 ),
-              ],
-            ),
+                value: flutterCourse,
+                onChanged: ((value) {
+                  flutterCourse = value!;
+                  setState(() {});
+                }),
+              ),
+              //! CHECKBOX #2
+              CheckboxListTile(
+                secondary: const Icon(
+                  Icons.book,
+                  color: Colors.black12,
+                ),
+                subtitle: const Text(
+                    "Based on the Typescript programming language.",
+                    style: TextStyle(fontSize: 20, color: Colors.black54)),
+                title: const Text(
+                  "React",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                ),
+                value: reactCourse,
+                onChanged: ((value) {
+                  reactCourse = value!;
+                  setState(() {});
+                }),
+              ),
+              if (flutterCourse)
+                const Image(
+                  image: NetworkImage(
+                      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Google-flutter-logo.svg/2560px-Google-flutter-logo.svg.png"),
+                ),
+              if (reactCourse)
+                const Image(
+                  image: NetworkImage(
+                      "https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png"),
+                ),
+            ],
           ),
         ),
       ),
